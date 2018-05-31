03a7395968fc7f29c4529a21344a72750362121e
Louay Alakkad <louay@alakkad.me>
2018-05-31 23:14:09 +0100

    Reorganise files

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
-package uptimerobot
+package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
+	"github.com/louy/terraform-provider-uptimerobot/uptimerobot"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
-			return Provider()
+			return uptimerobot.Provider()
 		},
 	})
 }
