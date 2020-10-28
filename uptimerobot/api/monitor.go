cc8fb4d89d93da11a927580a87e81287b2579803
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2020-10-29 00:05:12 +0900

    sort alert_contacts

--- uptimerobot/api/monitor.go
+++ uptimerobot/api/monitor.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"net/url"
+	"sort"
 	"strings"
 )
 
@@ -158,6 +159,9 @@ func (client UptimeRobotApiClient) GetMonitor(id int) (m Monitor, err error) {
 			ac.Threshold = int(contact["threshold"].(float64))
 			m.AlertContacts[k] = ac
 		}
+		sort.Slice(m.AlertContacts, func(i, j int) bool {
+			return m.AlertContacts[i].ID < m.AlertContacts[j].ID
+		})
 	}
 
 	return
